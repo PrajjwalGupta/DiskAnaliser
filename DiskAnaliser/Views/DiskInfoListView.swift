f28952f21de712b79a4ed46c3c3e133ea2d1322d
Prajjwal Gupta <31537066+PrajjwalGupta@users.noreply.github.com>
2025-09-08 18:56:27 +0530

    msg

---
+++ DiskAnaliser/Views/DiskInfoListView.swift
@@ -0,0 +1,18 @@
+//
+//  DiskInfoListView.swift
+//  DiskAnaliser
+//
+//  Created by Prajjwal on 08/09/25.
+//
+
+import SwiftUI
+
+struct DiskInfoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiskInfoListView()
+}
---
+++ DiskAnaliser/Views/DiskInfoRow.swift
@@ -0,0 +1,18 @@
+//
+//  DiskInfoRow.swift
+//  DiskAnaliser
+//
+//  Created by Prajjwal on 08/09/25.
+//
+
+import SwiftUI
+
+struct DiskInfoRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiskInfoRow()
+}
