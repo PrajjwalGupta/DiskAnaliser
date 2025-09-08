6de5286bcde7a8c008d48c21cdc9e481f8a0b00a
Prajjwal Gupta <31537066+PrajjwalGupta@users.noreply.github.com>
2025-09-08 15:19:51 +0530

    Initial Commit

---
+++ DiskAnaliser/DiskAnaliserApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiskAnaliserApp.swift
+//  DiskAnaliser
+//
+//  Created by Prajjwal on 08/09/25.
+//
+
+import SwiftUI
+
+@main
+struct DiskAnaliserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
